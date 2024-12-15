417c1920c08252591ba2b6fe585908704e5d2003
dishashetty546 <dishashetty546@gmail.com>
2024-12-15 09:25:17 +0530

    database connected

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,17 @@ const port = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   console.log("API working");
 });
+//database connection
+
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Mogodb database is connected");
+  } catch (err) {
+    console.log("mongodb database connected is failed");
+  }
+};
 
 //middelware
 app.use(express.json());
@@ -19,5 +30,6 @@ app.use(cors);
 app.use(cookieParser);
 
 app.listen(port, () => {
+  connectDB();
   console.log(`server is running in ${port}`);
 });
